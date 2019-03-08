19ed0a97c860f1518d09307f70194df28bedb476
rithiksingh098 <rithiksingh098@gmail.com>
2019-03-08 12:37:42 +0530

    app1 contains the project not app

---
+++ app 1.js
@@ -0,0 +1,85 @@
+var express=require("express");
+var app=express();
+app.set("views","./views");
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}));
+//for course i will have to make a
+//course are fixed cao, dsa, os, networks, iwp
+var courses=["cao","dsa","os","networks","iwp"];
+//course page shows inactive and active
+var nofstu=[0,0,0,0,0];
+app.use(express.urlencoded({extended:true}));
+var names=['rithik','ankita','akansha'];
+var pass=['1234','123','12'];
+var stucourses=[[],[],[]];
+app.use(function(req,res,next){
+    console.log("middleware");
+    console.log(req.url);
+    console.log(req.query);
+    if(req.query.name=='rithik'&&req.query.password=='1234'){
+        next();
+    }
+    else{
+        throw new Error("invaliid");
+        
+    }
+});
+app.get("/student",function(req,res){
+    console.log("get student");
+
+    console.log(req.query);
+    console.log(req.url);
+    if(req.query.name=='rithik'){
+        res.render("htmlfile2",{
+            courses:stucourses[0]
+        });
+    }
+});
+app.get("/course",function(req,res){
+    console.log("get course");
+
+    res.render("htmlfile",{
+        courses:courses,
+        nofstu:nofstu,
+        name:req.query.name,
+        password:req.query.password
+    });
+});
+app.post("/student",function(req,res){
+    console.log("post student");
+
+    console.log(req.query);
+
+    if(req.query.name=='rithik'){
+        res.render("htmlfile2",{
+            courses:stucourses[0]
+        });
+    }
+    
+});
+app.post("/course",function(req,res){
+    console.log("post course");
+
+    console.log(req.body);
+    console.log(req.url);
+    console.log(stucourses[0]);
+    nofstu[req.body.name1]++;
+    console.log(req.body.name1);
+    // if(req.body.name=='rithik')
+    {
+        stucourses[0].push(courses[parseInt(req.body.name1)]);
+    }
+    res.render("htmlfile",{
+        courses:courses,
+        nofstu:nofstu,
+        name:req.query.name,
+        password:req.query.password
+    });
+});
+
+//now i have to make a login fixed password and id
+//now the person logged in will have its own array and the 
+//subjects it will register will add to the array and finally 
+//the route student will tell the courses registerd it has..
+
+app.listen(3000);
